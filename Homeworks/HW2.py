f9c54e8b9c8fdc0fcf0e4dc7b87a863e01844aad
frknksknklc <52262374+frknksknklc@users.noreply.github.com>
2021-03-12 23:20:29 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,54 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+#CV Application
+Cv1 = {
+    "name":"Furkan",
+    "surname":"Kılıç",
+    "gender":"male",
+    "school":"Gazi University",
+    "driving license type":"B",
+    "hobbies":"swimming",
+    "certificate":"Python Certificate"
+    
+}
+Cv2 = {
+    "name":"Melike",
+    "surname":"Koç",
+    "gender":"female",
+    "school":"Ankara University",
+    "driving license type":"B",
+    "hobbies":"table tennis",
+    "certificate":"C# Certificate"
+    
+}
+Cv3 = {
+    "name":"Selim",
+    "surname":"Yıldız",
+    "gender":"male",
+    "school":"Harvard University",
+    "driving license type":"B",
+    "hobbies":"football",
+    "certificate":"Java Certificate"
+    
+}
+Cv4 = {
+    "name":"Pelin",
+    "surname":"Kaya",
+    "gender":"female",
+    "school":"Bilkent University",
+    "driving license type":"B",
+    "hobbies":"chess",
+    "certificate":"C++ Certificate"
+    
+}
+Cv5 = {
+    "name":"Faruk",
+    "surname":"Sel",
+    "gender":"male",
+    "school":"Sabancı University",
+    "driving license type":"B",
+    "hobbies":"travelling",
+    "certificate":"Javascript Certificate"
+    
+}
+list = [Cv1,Cv2,Cv3,Cv4,Cv5]
+for i in list:
+    print(i)
